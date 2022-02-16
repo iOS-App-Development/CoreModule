43f41f38479c77fce7afaad71249cf0f26adfe24
Muzammil Peer <muzammilpeer987@gmail.com>
2022-02-17 00:10:17 +0400

    general import added to main class

--- Sources/CoreModule/CoreModule.swift
+++ Sources/CoreModule/CoreModule.swift
@@ -1,6 +1,21 @@
-public struct CoreModule {
+import Foundation
+import APIClient
+import APIClientModule
+import HTTPClientModule
+
+public class CoreModule {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    
+}
+extension CoreModule:CoreModuleAPI
+{
+    func load() {
+    }
+    
+    func unload() {
     }
 }
---
+++ Sources/CoreModule/CoreModuleAPI.swift
@@ -0,0 +1,13 @@
+//
+//  CoreModuleAPI.swift
+//  
+//
+//  Created by Muzammil Peer on 17/02/2022.
+//
+
+import Foundation
+
+protocol CoreModuleAPI {
+    func load()
+    func unload()
+}
